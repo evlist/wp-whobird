c6fa8cc5e071b1c06220953d0f3ce47253ddfbee
Eric van der Vlist <vdv@dyomedea.com>
2025-04-22 16:07:12 +0200

    indentation

--- src/wp-whobird/render.php
+++ src/wp-whobird/render.php
@@ -10,109 +10,109 @@ use DateTime;
 use SQLite3;
 
 if (! class_exists('WPWhoBird\WhoBirdRenderer')) {
-        class WhoBirdRenderer
-        {
+    class WhoBirdRenderer
+    {
         private string $recordingsPath;
         private string $databasePath;
 
         public function __construct()
         {
-        // Retrieve the dynamic values from the WordPress options
-        $this->recordingsPath = get_option('wp-whobird_recordings_path', 'WhoBird/recordings');
-        $this->databasePath = get_option('wp-whobird_database_path', 'WhoBird/databases/BirdDatabase.db');
+            // Retrieve the dynamic values from the WordPress options
+            $this->recordingsPath = get_option('wp-whobird_recordings_path', 'WhoBird/recordings');
+            $this->databasePath = get_option('wp-whobird_database_path', 'WhoBird/databases/BirdDatabase.db');
         }
 
         private function getDatabaseConnection(): SQLite3
         {
-        $dbPath = wp_get_upload_dir()['basedir'] . '/' . $this->databasePath;
+            $dbPath = wp_get_upload_dir()['basedir'] . '/' . $this->databasePath;
 
-        $db = new SQLite3($dbPath, SQLITE3_OPEN_READONLY);
-        if (!$db) {
-        throw new \Exception(__('Failed to open database at', 'wp-whobird') . ' ' . $dbPath);
-        }
+            $db = new SQLite3($dbPath, SQLITE3_OPEN_READONLY);
+            if (!$db) {
+                throw new \Exception(__('Failed to open database at', 'wp-whobird') . ' ' . $dbPath);
+            }
 
-        return $db;
+            return $db;
         }
 
         private function getStartTime(): int
-            {
-                $isoDate = get_the_date('Y-m-d');
-                $date = new DateTime($isoDate);
-                return $date->getTimestamp() * 1000; // Convert to milliseconds
-            }
+        {
+            $isoDate = get_the_date('Y-m-d');
+            $date = new DateTime($isoDate);
+            return $date->getTimestamp() * 1000; // Convert to milliseconds
+        }
 
         private function getEndTime(): int
-            {
-                return $this->getStartTime() + 24 * 60 * 60 * 1000; // Add 24 hours in milliseconds
-            }
+        {
+            return $this->getStartTime() + 24 * 60 * 60 * 1000; // Add 24 hours in milliseconds
+        }
 
         private function getRecordingsUrls(string $recordingIdsString): string
-            {
-                $recordingIds = explode(',', $recordingIdsString);
-                $recordingUrls = [];
-                $uploadDir = wp_get_upload_dir();
+        {
+            $recordingIds = explode(',', $recordingIdsString);
+            $recordingUrls = [];
+            $uploadDir = wp_get_upload_dir();
 
-                foreach ($recordingIds as $recordingId) {
-                    $recordingPath = $uploadDir['basedir'] . '/' . $this->recordingsPath . '/' . $recordingId . '.wav';
-                    if (is_file($recordingPath) && is_readable($recordingPath)) {
-                        $recordingUrl = $uploadDir['baseurl'] . '/' . $this->recordingsPath . '/' . $recordingId . '.wav';
-                        $recordingUrls[] = $recordingUrl;
-                    }
+            foreach ($recordingIds as $recordingId) {
+                $recordingPath = $uploadDir['basedir'] . '/' . $this->recordingsPath . '/' . $recordingId . '.wav';
+                if (is_file($recordingPath) && is_readable($recordingPath)) {
+                    $recordingUrl = $uploadDir['baseurl'] . '/' . $this->recordingsPath . '/' . $recordingId . '.wav';
+                    $recordingUrls[] = $recordingUrl;
                 }
-
-                return implode(',', $recordingUrls);
             }
+
+            return implode(',', $recordingUrls);
+        }
 
         private function getObservationsList(): string
-            {
-                $db = $this->getDatabaseConnection();
-                $startTime = $this->getStartTime();
-                $endTime = $this->getEndTime();
+        {
+            $db = $this->getDatabaseConnection();
+            $startTime = $this->getStartTime();
+            $endTime = $this->getEndTime();
 
-                // Get the threshold value from the admin settings
-                $threshold = floatval(get_option('wp-whobird_threshold', 0.7));
+            // Get the threshold value from the admin settings
+            $threshold = floatval(get_option('wp-whobird_threshold', 0.7));
 
-                $query = $db->prepare(
-                        "SELECT BirdNET_ID, SpeciesName, group_concat(TimeInMillis) as timestamps 
-                        FROM BirdObservations 
-                        WHERE TimeInMillis >= :startTime AND TimeInMillis < :endTime AND Probability >= :threshold 
-                        GROUP BY BirdNET_ID, SpeciesName"
-                        );
+            $query = $db->prepare(
+                    "SELECT BirdNET_ID, SpeciesName, group_concat(TimeInMillis) as timestamps 
+                    FROM BirdObservations 
+                    WHERE TimeInMillis >= :startTime AND TimeInMillis < :endTime AND Probability >= :threshold 
+                    GROUP BY BirdNET_ID, SpeciesName"
+                    );
 
-                $query->bindValue(':startTime', $startTime, SQLITE3_INTEGER);
-                $query->bindValue(':endTime', $endTime, SQLITE3_INTEGER);
-                $query->bindValue(':threshold', $threshold, SQLITE3_FLOAT);
+            $query->bindValue(':startTime', $startTime, SQLITE3_INTEGER);
+            $query->bindValue(':endTime', $endTime, SQLITE3_INTEGER);
+            $query->bindValue(':threshold', $threshold, SQLITE3_FLOAT);
 
-                $results = $query->execute();
+            $results = $query->execute();
 
-                $list = '';
-                while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-                    $list .= '<li data-recordings="' . esc_attr($this->getRecordingsUrls($row['timestamps'])) . '">';
-                    $list .= esc_html($row['SpeciesName']);
-                    $list .= '</li>';
-                }
-
-                return $list;
+            $list = '';
+            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+                $list .= '<li data-recordings="' . esc_attr($this->getRecordingsUrls($row['timestamps'])) . '">';
+                $list .= esc_html($row['SpeciesName']);
+                $list .= '</li>';
             }
+
+            return $list;
+        }
 
         public function displayObservations(): void
-            {
-                $list = $this->getObservationsList();
+        {
+            $list = $this->getObservationsList();
 
-                if (empty($list)) {
-                    echo '<div class="wpwbd_observations wpwbd_empty_observations">';
-                    echo '<p>' . __('We did not identify any birds with the WhoBIRD application today.', 'wp-whobird') . '</p>';
-                    echo '</div>';
-                } else {
-                    echo '<div class="wpwbd_observations">';
-                    echo '<p>' . __('Today we heard and identified the following birds with the WhoBIRD application:', 'wp-whobird') . '</p>';
-                    echo '<ul class="wpwbd_list">';
-                    echo $list;
-                    echo '</ul>';
-                    echo '</div>';
-                }
+            if (empty($list)) {
+                echo '<div class="wpwbd_observations wpwbd_empty_observations">';
+                echo '<p>' . __('We did not identify any birds with the WhoBIRD application today.', 'wp-whobird') . '</p>';
+                echo '</div>';
+            } else {
+                echo '<div class="wpwbd_observations">';
+                echo '<p>' . __('Today we heard and identified the following birds with the WhoBIRD application:', 'wp-whobird') . '</p>';
+                echo '<ul class="wpwbd_list">';
+                echo $list;
+                echo '</ul>';
+                echo '</div>';
             }
         }
+    }
 }
 
 // Initialize and render the observations
