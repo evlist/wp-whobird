cbd46af612db25451a9b0bedcfe316bd2668497a
Eric van der Vlist <vdv@dyomedea.com>
2025-05-13 20:55:25 +0200

    More adjustments (#3)

--- src/wp-whobird/lib/WikidataQuery.php
+++ src/wp-whobird/lib/WikidataQuery.php
@@ -50,19 +50,19 @@ class WikidataQuery {
             return $cachedData['data'];
         }
 
-        $currentTime = microtime(true) / 1000; // Current time in milliseconds
+        $currentTime = microtime(true) * 1000; // Current time in milliseconds
         $timeSinceLastRequest = $currentTime - self::$lastRequestTime;
 
         if ($timeSinceLastRequest < self::$requestIntervalMs) {
-            usleep((self::$requestIntervalMs - $timeSinceLastRequest) * 1000); // Convert ms to microseconds
+           usleep((self::$requestIntervalMs - $timeSinceLastRequest) * 1000); // Convert ms to microseconds
         }
 
+        self::$lastRequestTime = microtime(true) * 1000; // Update the last request time to the current time in milliseconds
         $sparqlUrl = "https://query.wikidata.org/sparql?query=" . urlencode($this->buildSparqlQuery($ebirdId));
         $sparqlHeaders = ["Accept: application/json"];
         $sparqlResponse = $this->executeCurl($sparqlUrl, $sparqlHeaders);
         $sparqlData = json_decode($sparqlResponse, true);
 
-        self::$lastRequestTime = microtime(true) / 1000; // Update the last request time to the current time in milliseconds
 
         return $this->processAndCacheData($ebirdId, $sparqlData);
     }
